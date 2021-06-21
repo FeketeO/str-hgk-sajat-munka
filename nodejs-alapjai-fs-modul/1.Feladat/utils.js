533e8cde1752f30c2d66d5603e1016b02fdf1dc7
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-06-21 23:25:27 +0200

    1 fealdat + tippek

--- nodejs-alapjai-fs-modul/1.Feladat/utils.js
+++ nodejs-alapjai-fs-modul/1.Feladat/utils.js
@@ -11,3 +11,58 @@ const fileCreator = () => {
 }
 
 fileCreator()
+
+// NEM ÍGY OLDOTTAM MEG, DE NAGYON TETSZIK:
+
+// utils.js.
+
+// const { mkdir, writeFile } = require('fs').promises
+
+// const createStarterTemplate = (folders, files) => {
+//     folders.map(folder => mkdir(folder))
+//     files.map(file => writeFile(file, ''))       
+// }
+
+// module.exports = createStarterTemplate
+
+// utilsmasik.js
+// const createStarterTemplate = require('./utils')
+// const folders = ['controllers', 'routers', 'views']
+
+// const files = [
+//   './controllers/site.controller.js',
+//   './routers/site.router.js',
+//   './views/index.html',
+//   './app.js'
+// ]
+// createStarterTemplate(folders,files) 
+
+// VAGY:
+// utils:
+// const { mkdir, writeFile } = require('fs').promises;
+
+// const createFolders = async (folders) =>
+//   await folders.map( folder => mkdir(folder) );
+
+// const createFiles = async (files) =>
+//   await files.map( file => writeFile(file, '') );
+
+// module.exports = {
+//     createFolders, 
+//     createFiles
+// };
+
+// utilsmasik:
+// const { createFolders, createFiles } = require('./utils');
+
+// const folders = ['controllers', 'routers', 'views'];
+
+// const files = [
+//   './controllers/site.controller.js',
+//   './routers/site.router.js',
+//   './views/index.html',
+//   './app.js'
+// ];
+
+// createFolders(folders)
+//   .then(() => createFiles(files));
